ee4cf6972b4a40f3163a1246ab3c0b17120696df
Endynijs <Endy@nijs.tech>
2025-04-16 07:36:20 +0200

    Add files via upload

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -7,7 +7,7 @@ export default function Homepage() {
     <div className="bg-[#0e141e] text-white font-sans">
       <header className="flex justify-between items-center p-6 max-w-7xl mx-auto">
         <div className="text-xl font-bold">
-          <img src="/nijs-logo-cropped.png" alt="Nijs Logo" className="h-40" />
+          <img src="/nijs-logo-cropped.png" alt="Nijs Logo" className="h-20" />
         </div>
         <nav className="space-x-6 text-sm text-white/80">
           <a href="#diensten">Diensten</a>
